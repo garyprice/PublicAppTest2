88a2ef899015e17cee158452f97d912c09a7aae6
Gary Price <gary@garyprice.london>
2020-11-05 14:29:52 +0000

    Local git change

--- PublicAppTest2/PublicAppTest2/Program.cs
+++ PublicAppTest2/PublicAppTest2/Program.cs
@@ -6,7 +6,7 @@ namespace PublicAppTest2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
